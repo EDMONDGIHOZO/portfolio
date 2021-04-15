c13a3b0247c9afc1518446c9ddc9abbee750c145
EDMONDGIHOZO <gihozoedmond@gmail.com>
2021-04-15 17:57:20 +0200

    done registration view and template

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
+    path("", include("users.urls")),
 ]
---
+++ users/urls.py
@@ -0,0 +1,4 @@
+from django.conf.urls import url
+from users.views import dashboard
+
+urlpatterns = []
